a8f9069acfa1a0855a296a13e85c9b33b5c1d56d
Yan Wallas <yanwallas98@gmail.com>
2025-01-08 20:28:39 -0300

    Adiciona corretamente a pasta meuapp

---
+++ meuapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Bemvindo = () => <h2>Bem Vindo(a)</h2>;
+
+export default function App() {
+  return (
+    <div className="App">
+      Olá mundo!
+      <Bemvindo />
+      <h1> Bem Vindo ao sistema</h1>
+      <h2> Curso React </h2>vfvf
+    </div>
+  );
+}
