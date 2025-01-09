6acb001f40b997f8c51fbdd97ff38304983334aa
Yan Wallas <yanwallas98@gmail.com>
2025-01-09 18:41:14 -0300

    Usando states

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,66 +1,47 @@
 import React, { Component } from 'react';
 
-class Equipe extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: 'Matheus',
+      contador: 0,
+    };
+
+    this.aumentar = this.aumentar.bind(this);
+    this.diminuir = this.diminuir.bind(this);
+  }
+
+  aumentar() {
+    let state = this.state;
+    state.contador += 1;
+    this.setState(state);
+  }
+
+  diminuir() {
+    let state = this.state;
+    if (state.contador === 0) {
+      alert('Opa!! chegou a zero!');
+      return;
+    }
+
+    state.contador -= 1;
+    this.setState(state);
+  }
+
   render() {
     return (
       <div>
-        <Sobre
-          nome={this.props.nome}
-          cargo={this.props.cargo}
-          idade={this.props.idade}
-        />
-        <Social />
-        <hr />
+        <h1>Contador</h1>
+
+        <h3>
+          <button onClick={this.diminuir}>-</button>
+          {this.state.contador}
+          <button onClick={this.aumentar}>+</button>
+        </h3>
       </div>
     );
   }
-}
-
-class Sobre extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Olá sou o(a) {this.props.nome} </h2>
-        <h3>Cargo: {this.props.cargo} </h3>
-        <h3>Idade: {this.props.idade} anos </h3>
-      </div>
-    );
-  }
-}
-
-const Social = () => {
-  return (
-    <div>
-      <a>Facebook</a>
-      <a>Instagram</a>
-    </div>
-  );
-};
-
-function App() {
-  return (
-    <div>
-      <h1>Conheça nossa equipe:</h1>
-      <Equipe
-        nome="Lucas"
-        cargo="Programador"
-        idade="29"
-        facebook="https://google.com"
-      />
-      <Equipe
-        nome="Lima"
-        cargo="T.I"
-        idade="27"
-        facebook="https://google.com"
-      />
-      <Equipe
-        nome="Amanda"
-        cargo="Front-End"
-        idade="22"
-        facebook="https://google.com"
-      />
-    </div>
-  );
 }
 
 export default App;
