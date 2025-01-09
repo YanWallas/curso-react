54f509d5b8e6eeab8fd67287fad6901b13a3a226
Yan Wallas <yanwallas98@gmail.com>
2025-01-09 14:30:00 -0300

    Update App.js

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Bemvindo = (props) => {
+  // Conhecendo Props.
   return (
     <div>
       <h2> Bem Vindo(a) {props.nome} </h2>
+      <h3> Tenho {props.idade} Anos </h3>
     </div>
   );
 };
@@ -12,7 +14,12 @@ export default function App() {
   return (
     <div className="App">
       Olá mundo!
-      <Bemvindo nome="yan" />
+      <Bemvindo
+        nome="Yan"
+        idade="26" /* Rederizando Const Bem vindo, com os props */
+      />
+      <Bemvindo nome="Lima" idade="27" />
+      <Bemvindo nome="Maria" idade="24" />
       <h2> Curso React </h2>
     </div>
   );
