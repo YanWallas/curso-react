846b9e3a9b2ca7f857340abadbe933f1f54913f4
Yan Wallas <yanwallas98@gmail.com>
2025-01-13 18:25:09 -0300

    renderização condicional status

--- meuapp/src/componentes/Membro/index.js
+++ meuapp/src/componentes/Membro/index.js
@@ -4,14 +4,35 @@ class Membro extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      status: 1,
+      status: false,
     };
+
+    this.sair = this.sair.bind(this);
+    this.entrar = this.entrar.bind(this);
+  }
+
+  sair() {
+    this.setState({ status: false });
+  }
+
+  entrar() {
+    this.setState({ status: true });
   }
 
   render() {
     return (
       <div>
-        {this.state.status === 1 && <h2>Bem-vindo(a) ao Sistema!!</h2>}
+        {this.state.status ? (
+          <div>
+            <h2>Bem-vindo(a) ao Sistema!!</h2>
+            <button onClick={this.sair}>Sair do Sistema</button>
+          </div>
+        ) : (
+          <div>
+            <h2>Olá visitante, faça o login!</h2>
+            <button onClick={this.entrar}>Entrar no Sistema</button>
+          </div>
+        )}
 
         <h2>Curso React JS</h2>
       </div>
