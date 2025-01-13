1fb82ca91e3e5b7b9a7291b7b8b2d877bd4d4433
Yan Wallas <yanwallas98@gmail.com>
2025-01-13 17:18:58 -0300

    Aula 40 - Eventos

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,34 +1,11 @@
 import React, { Component } from 'react';
+import Membro from './componentes/Membro';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hora: '00:00:00',
-    };
-  }
-
-  componentDidMount() {
-    // componente montado....
-    setInterval(() => {
-      this.setState({ hora: new Date().toLocaleTimeString() });
-    }, 1000);
-  }
-
-  componentDidUpdate() {
-    //Verificar update de algum componentDidMount, Quando verifica, pode escolher fazer algo,
-    console.log('Atualizou!!!!');
-  }
-
-  shouldComponentUpdate() {
-    //return true ou false
-    console.log('OK!!!!');
-  }
-
   render() {
     return (
       <div>
-        <h1>Meu Projeto {this.state.hora} </h1>
+        <Membro />
       </div>
     );
   }
