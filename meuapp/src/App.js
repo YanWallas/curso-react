242fcac7338ba43bf86a63084450519eafb0a442
Yan Wallas <yanwallas98@gmail.com>
2025-01-15 16:41:41 -0300

    Trabalhando com lista

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-import Membro from './componentes/Membro';
+import Feed from './componentes/feed';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      feed: [
+        { id: 1, username: 'Lima', curtidas: 10, comentarios: 2 },
+        { id: 2, username: 'Lucas', curtidas: 120, comentarios: 24 },
+        { id: 3, username: 'Amanda', curtidas: 30, comentarios: 12 },
+        { id: 4, username: 'Wallas', curtidas: 1, comentarios: 1 },
+      ],
+    };
+  }
+
   render() {
     return (
       <div>
-        <Membro />
+        {this.state.feed.map((item) => {
+          return (
+            <Feed
+              id={item.id}
+              username={item.username}
+              curtidas={item.curtidas}
+              comentarios={item.comentarios}
+            />
+          );
+        })}
       </div>
     );
   }
