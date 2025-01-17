15a3d045a9bfbae6d3aafe3be24c2054521f04df
Yan Wallas <yanwallas98@gmail.com>
2025-01-17 17:00:55 -0300

    Update App.js

--- sobre-hooks/src/App.js
+++ sobre-hooks/src/App.js
@@ -4,14 +4,16 @@ function App() {
 
   const [tarefas, setTarefas] = useState(['Pagar a conta de luz', 'Estudar React Hooks']);
 
+  const [input, setInput] = useState('');
+
   function handleAdd(){
-    alert('Olá')
+    setTarefas = ([...tarefas, input])
   }
 
   return (
     <div>
       <ul>{tarefas.map(tarefa => (<li key={tarefa}>{tarefa}</li>))}</ul>
-
+      <input type='text' value={input} onChange={e => setInput(e.target.value)}/>
       <button type="button" onClick={handleAdd}>Adicionar</button>
     </div>
   );
