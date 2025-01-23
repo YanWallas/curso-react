feda09b862a490651eda86906fdd0a5ce2f3b29a
Yan Wallas <yanwallas98@gmail.com>
2025-01-23 18:11:56 -0300

    create Home

--- prime-filmes/src/Pages/Home/index.js
+++ prime-filmes/src/Pages/Home/index.js
@@ -1,4 +1,29 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
+// URL da api: /movie/now_playing?api_key=10336075e69e824f4134ca90376b9ed8
+
 function Home(){
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(()=> {
+    async function loasdFilmes() {
+      const response = await api.get("movie/now_playing", {
+        params:{
+          api_key: "10336075e69e824f4134ca90376b9ed8",
+          language: "pt-BR",
+          page: 1,
+        }
+      })
+
+      console.log(response.data.results);
+    }
+
+    loasdFilmes();
+
+  },[])
+
+
   return(
     <div>
       <h1>Bem Vindo a Home</h1>
