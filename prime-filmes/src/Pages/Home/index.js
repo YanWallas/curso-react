9012d9750362934a2ddfc08e76d46db48d1c8a9d
Yan Wallas <yanwallas98@gmail.com>
2025-01-27 16:37:29 -0300

    Detalhes do filmes pt.3

--- prime-filmes/src/Pages/Home/index.js
+++ prime-filmes/src/Pages/Home/index.js
@@ -20,7 +20,7 @@ function Home(){
       })
 
       //console.log(response.data.results.slice(0, 10));
-      setFilmes(response.data.results.slice(0, 10));
+      setFilmes(response.data.results);
       setLoading(false);
     }
 
