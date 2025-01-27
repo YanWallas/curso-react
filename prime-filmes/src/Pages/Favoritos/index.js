509ee0bbf2958d43f99b405eb3e8bf5b4cf97758
Yan Wallas <yanwallas98@gmail.com>
2025-01-27 17:54:58 -0300

    Lista Filmes

--- prime-filmes/src/Pages/Favoritos/index.js
+++ prime-filmes/src/Pages/Favoritos/index.js
@@ -3,16 +3,27 @@ import './favoritos.css';
 import { Link } from 'react-router-dom';
 
 function Favoritos(){
-  const [filmes, setfilmes] = useState([])
+  const [filmes, setFilmes] = useState([])
 
   useEffect(() => {
     const minhaLista = localStorage.getItem("@primeflix");
-    setfilmes(JSON.parse(minhaLista) || [])
+    setFilmes(JSON.parse(minhaLista) || [])
   }, [])
+
+  function excluirFilme(id){
+    let filtroFilmes = filmes.filter((item) => {
+      return(item.id !== id)
+    })
+
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes))
+  }
 
   return(
     <div className='meus-filmes'>
       <h1>Meus Filmes</h1>
+
+      {filmes.length === 0 && <span>Você não possui nenhum filme salvo :( </span>}
 
       <ul>
         {filmes.map((item) => {
@@ -22,7 +33,7 @@ function Favoritos(){
 
               <div>
                 <Link to={`/filme/${item.id}`}>Ver Detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => excluirFilme(item.id) }>Excluir</button>
               </div>
             </li>
           )
