b3566ea9e4e7147b7a0331072d63c3c51be0f8b1
Yan Wallas <yanwallas98@gmail.com>
2025-01-29 16:40:22 -0300

    Trabalhando  com listas

--- fireapp/src/App.js
+++ fireapp/src/App.js
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import { db } from './firebasec';
-import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore';
+import { doc, setDoc, collection, addDoc, getDoc, getDocs } from 'firebase/firestore';
 import './app.css';
 
 function App() {
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
 
+  const [posts, setPosts] = useState([]);
+
   async function handleAdd(){
+    // ADICIONANDO REGISTRADO NA MAO 
     // await setDoc(doc(db, "posts", "12345"), {
     //   titulo: titulo,
     //   autor: autor,
@@ -31,14 +34,35 @@ function App() {
   }
 
   async function buscarPost() {
-    const postRef = doc(db, "posts", "12345")
+    // BUSCANDO POST ESPECIFICO (NA MAO)
+    // const postRef = doc(db, "posts", "12345")
 
-    await getDoc(postRef)
-    .then((snapshot)=> {
-      setAutor(snapshot.data().autor)
-      setTitulo(snapshot.data().titulo)
+    // await getDoc(postRef)
+    // .then((snapshot)=> {
+    //   setAutor(snapshot.data().autor)
+    //   setTitulo(snapshot.data().titulo)
+    // })
+    // .catch(()=>{console.log("ERRO AO BUSCAR")})
+
+    const postsRef = collection(db, "posts")
+    await getDocs(postsRef)
+    .then((snapshot) => {
+      let lista = [];
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor,
+        })
+      })
+
+      setPosts(lista);
     })
-    .catch(()=>{console.log("ERRO AO BUSCAR")})
+    .catch((error) => {
+      console.log("DEU ALGUM ERRO AO BUSCAR" + error)
+    })
+
   }
 
   return (
@@ -64,6 +88,17 @@ function App() {
 
         <button onClick={handleAdd}>Cadastrar</button>
         <button onClick={buscarPost}>Buscar Cadastros</button>
+
+        <ul>
+          {posts.map((post) => {
+            return(
+              <li key={post.id}>
+                <span>Titulo: {post.titulo}</span><br/>
+                <span>Autor: {post.autor}</span><br/><br/>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </div>
   );
