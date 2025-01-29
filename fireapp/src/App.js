fd6f4662729cd7d21fdbd4ae6974a3cf300873ac
Yan Wallas <yanwallas98@gmail.com>
2025-01-29 17:31:56 -0300

    Conhecendo onSnapshot

--- fireapp/src/App.js
+++ fireapp/src/App.js
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { db } from './firebasec';
-import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc, deleteDoc } from 'firebase/firestore';
+import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc, deleteDoc, onSnapshot } from 'firebase/firestore';
 import './app.css';
 
 function App() {
@@ -9,6 +9,26 @@ function App() {
   const [idPost, setIdPost] = useState('');
 
   const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function loadPosts() {
+      const unsub = onSnapshot(collection(db, "posts"), (snapshot) => {
+        let listaPost = [];
+
+      snapshot.forEach((doc) => {
+        listaPost.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor,
+        })
+      })
+
+      setPosts(listaPost);
+      })
+    }
+
+    loadPosts();
+  }, [])
 
   async function handleAdd(){
     // ADICIONANDO REGISTRADO NA MAO 
