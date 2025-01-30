d0a06149f3ea796250209ee29685adebbf01301e
Yan Wallas <yanwallas98@gmail.com>
2025-01-30 13:09:52 -0300

    Começando projeto lista de tarefas e ajustando route

---
+++ lista-fire/src/firebasec.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+const firebaseApp = initializeApp(firebaseConfig);//inicializando Firebase
+
+const db = getFirestore(firebaseApp);//get para ter a conexão com o banco.
+const auth = getAuth(firebaseApp);//Para acessar a conexaõ com autentificação de usuario.
+
+export { db, auth };
