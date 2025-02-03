c06aaa500179b3066f4dd030af16ff983ad7f765
Yan Wallas <yanwallas98@gmail.com>
2025-02-03 18:52:42 -0300

    Pagina Login pt1

--- sistema-chamados/src/pages/SignIn/index.js
+++ sistema-chamados/src/pages/SignIn/index.js
@@ -1,7 +1,40 @@
+import { useState } from 'react';
+import './signin.css';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
+
 export default function SignIn(){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] =  useState('');
+
   return(
-    <div>
-      <h1>Pagina login</h1>
+    <div className='container-center'>
+      <div className='login'>
+        <div className='login-area'>
+          <img src={logo} alt='Logo do sistema de chamadosc'/>
+        </div>
+
+        <form>
+          <h1>Entrar</h1>
+          <input
+            type='text'
+            placeholder='email@email.com'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          
+          <input
+            type='password'
+            placeholder='******'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <input type='submit' value="Acessar"/>
+        </form>
+
+        <Link to="/register">Criar uma conta</Link>
+      </div>
     </div>
   )
 }
