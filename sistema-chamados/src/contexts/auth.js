0703d26e3ceca6f80324ea6025660673b92a283a
Yan Wallas <yanwallas98@gmail.com>
2025-02-04 14:55:44 -0300

    Cadastrando user pt2

--- sistema-chamados/src/contexts/auth.js
+++ sistema-chamados/src/contexts/auth.js
@@ -2,12 +2,16 @@ import { useState, createContext, useEffect } from "react";
 import { auth, db } from '../services/firebase';
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }){
   const [user, setUser] = useState(null)
   const [loadingAuth, setLoadingAuth] = useState(false);
+
+  const navigate = useNavigate();
 
   function signIn(email, password){
     console.log(email);
@@ -35,13 +39,20 @@ function AuthProvider({ children }){
           avatarUrl: null
         };
         setUser(data);
+        storageUser(data);
         setLoadingAuth(false);
+        toast.success("Seja bem-vindo(a) ao sistema!")
+        navigate("/dashboard")
       })
     })
     .catch((error) => {
       console.log(error);
       setLoadingAuth(false);
     })
+  }
+
+  function storageUser(data){
+    localStorage.setItem('@ticketsPRO', JSON.stringify(data))
   }
 
   return(
