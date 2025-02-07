7c207eabe269382d1d41f71c47ab37b16d507bd1
Yan Wallas <yanwallas98@gmail.com>
2025-02-07 12:38:23 -0300

    estilizando tabela

--- sistema-chamados/src/pages/Dashboard/index.js
+++ sistema-chamados/src/pages/Dashboard/index.js
@@ -1,5 +1,3 @@
-import { useContext } from "react";
-import { AuthContext } from "../../contexts/auth";
 import Header from "../../components/Header";
 import Title from '../../components/Title';
 import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from "react-icons/fi";
@@ -7,12 +5,6 @@ import { Link } from "react-router-dom";
 import './dashboard.css';
 
 export default function Dashboard(){
-  const { logout } = useContext(AuthContext);
-
-  async function handleLogout(){
-    await logout();
-  }
-
   return(
     <div>
       <Header/>
@@ -41,7 +33,26 @@ export default function Dashboard(){
               <tr>
                 <td data-label="Cliente">mercado</td>
                 <td data-label="Assunto">Suporte</td>
-                <td data-label="Status">Em Aberto</td>
+                <td data-label="Status">
+                  <span className="badge" style={{ backgroundColor: '#999'}}>Em aberto</span>
+                </td>
+                <td data-label="Cadastrado">12/05/2022</td>
+                <td data-label="#">
+                  <button className="action" style={{ backgroundColor: '#3583f6' }}>
+                    <FiSearch color="#FFF" size={17}/>
+                  </button>
+                  <button className="action" style={{ backgroundColor: '#f6a935' }}>
+                    <FiEdit2 color="#FFF" size={17}/>
+                  </button>
+                </td>
+              </tr>
+
+              <tr>
+                <td data-label="Cliente">Informatica</td>
+                <td data-label="Assunto">Suporte</td>
+                <td data-label="Status">
+                  <span className="badge" style={{ backgroundColor: '#999'}}>Em aberto</span>
+                </td>
                 <td data-label="Cadastrado">12/05/2022</td>
                 <td data-label="#">
                   <button className="action" style={{ backgroundColor: '#3583f6' }}>
