ff3cd3a72493b207543868202357dd81863e45fb
Yan Wallas <yanwallas98@gmail.com>
2025-02-13 14:12:23 -0300

    Começando Projeto

---
+++ repos/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Main from './pages/Main';
+import Repositorio from './pages/Repositorio/';
+
+
+export default function RoutesApp(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' Component={Main}/>
+        <Route exact path='/repositorio/:repositorio' Component={Repositorio}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
