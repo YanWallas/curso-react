b90606e72e1e98b08199cc3255f7528b7f4b9060
Yan Wallas <yanwallas98@gmail.com>
2025-02-14 16:23:20 -0300

    Buscando repos

--- repos/src/pages/Main/index.js
+++ repos/src/pages/Main/index.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { FaGithub, FaPlus } from "react-icons/fa";
 import { Container, Form, SubmitButton } from "./styles";
+import api from '../services/api';
 
 export default function Main(){
+  const [newRepo, setNewRepo] = useState('');
+  const [Repositorios, setRepositorios] = useState([]);
+
+  function handleInputChange(e){
+    setNewRepo(e.target.value);
+  }
+
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+
+    async function submit(){
+      const response = await api.get(`repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name,
+      }
+      setRepositorios([...Repositorios, data]);
+      setNewRepo('');
+    }
+    
+    submit();
+  }, [newRepo, Repositorios]);
+    
   return(
     <Container>
       <h1>
@@ -10,8 +34,13 @@ export default function Main(){
         Meus Repositorios
       </h1>
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar Repositorio"/>
+      <Form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Adicionar Repositorio"
+          value={newRepo}
+          onChange={handleInputChange}
+        />
 
         <SubmitButton>
           <FaPlus color="#FFF" size={14}/>
