7ed727e8d9eb2c81ffe9a3fe1ebf77aeeead5df9
Yan Wallas <yanwallas98@gmail.com>
2025-02-17 12:22:53 -0300

    Detalhes do Repo

--- repos/src/pages/Repositorio/index.js
+++ repos/src/pages/Repositorio/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function Repositorio(){
+  let { repositorio } = useParams();
+
   return(
-    <h1>Repositorio</h1>
+    <h1 style={{color:'#FFF'}}>{repositorio}</h1>
   )
 }
