25b9658cdcc7e14bfb179146042ffca80bcb2c67
Yan Wallas <yanwallas98@gmail.com>
2025-02-17 13:08:40 -0300

    Carregando Detalhes

--- repos/src/pages/Repositorio/index.js
+++ repos/src/pages/Repositorio/index.js
@@ -1,10 +1,42 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { Container } from "./styles";
+import { useEffect, useState } from "react";
+import api from "../services/api";
 
 export default function Repositorio(){
   let { repositorio } = useParams();
 
+  const [repo, setRepo] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+  async function load(){
+    const nomeRepo = repositorio;
+
+    const [repoData, issuesData] = await Promise.all([
+      api.get(`/repos/${nomeRepo}`),
+      api.get(`/repos/${nomeRepo}/issues`, {
+        params:{
+          state: 'open',
+          per_page: 15
+        }
+      })
+    ]);
+
+    setRepo(repoData.data);
+    setIssues(issuesData.data);
+    setLoading(false);
+  }
+
+  load();
+
+  }, [repositorio]);
+
   return(
-    <h1 style={{color:'#FFF'}}>{repositorio}</h1>
+    <Container>
+
+    </Container>
   )
 }
