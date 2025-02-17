42359cadfa49bb6fbabb6c7e0d4ed0deab651599
Yan Wallas <yanwallas98@gmail.com>
2025-02-17 16:23:06 -0300

    Exibindo Issues

--- repos/src/pages/Repositorio/index.js
+++ repos/src/pages/Repositorio/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { Container, Owner, Loading, BackButton } from "./styles";
+import { Container, Owner, Loading, BackButton, IssuesList } from "./styles";
 import { useEffect, useState } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import api from "../services/api";
@@ -57,6 +57,26 @@ export default function Repositorio(){
         <h1>{repo.name}</h1>
         <p>{repo.description}</p>
       </Owner>
+
+      <IssuesList>
+        {issues.map(issue =>(
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login}/>
+
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
     </Container>
   )
 }
